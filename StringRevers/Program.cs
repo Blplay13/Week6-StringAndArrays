55fcbac87ed215852f312461addf89d8bec3c3ee
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-06 20:00:13 +0200

    Update Program.cs

--- StringRevers/Program.cs
+++ StringRevers/Program.cs
@@ -10,6 +10,17 @@ namespace StringRevers
     {
         static void Main(string[] args)
         {
+            //programm kuvab kasutaja eesnime tagurpidi
+
+            Console.WriteLine("Sisestama oma eesnimi:");
+            string firstName = Console.ReadLine();
+
+            for(int i = firstName.Length - 1; i >= 0; i--)
+            {
+                Console.Write(firstName[i]);
+            
+            }
         }
+        
     }
 }
