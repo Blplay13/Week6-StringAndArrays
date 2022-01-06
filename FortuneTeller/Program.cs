258908c86a15df2bf5c7f5c218bbe38137654f3e
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-06 19:56:20 +0200

    Update Program.cs

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -10,6 +10,13 @@ namespace FortuneTeller
     {
         static void Main(string[] args)
         {
+            //programm ennustab, mis juhtub kasutajaga homme (random)
+            string[] predictions = new string[4] { "win a million", "fall in love", "buy youtube premium", "join the dark side for cookies" };
+
+            Random rnd = new Random();
+            int userNumber = rnd.Next(0, predictions.Length);
+
+            Console.WriteLine($"Tomorrow you will {predictions[userNumber]}.");
         }
     }
 }
