18c686503a9aede54eebd9857d84ff9009a77ea1
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-06 19:58:01 +0200

    Update Program.cs

--- HalloWorldReverse/Program.cs
+++ HalloWorldReverse/Program.cs
@@ -10,6 +10,15 @@ namespace HalloWorldReverse
     {
         static void Main(string[] args)
         {
+            //programm kuvab lauset "Hello, World!" tagurpidi;
+
+            string HelloWorld = "Hello, World!";
+
+            for (int i = HelloWorld.Length - 1; i >= 0; i--)
+
+            {
+                Console.Write(HelloWorld[i]);
+            }
         }
     }
 }
