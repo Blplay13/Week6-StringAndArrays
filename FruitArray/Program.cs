aa4564d30703aaf6e00f117a41e24af0659b0228
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2022-01-06 19:57:10 +0200

    Update Program.cs

--- FruitArray/Program.cs
+++ FruitArray/Program.cs
@@ -10,6 +10,21 @@ namespace FruitArray
     {
         static void Main(string[] args)
         {
+            //programm ennustab, mis puuvilja kasutaja soob
+
+            //string[] fruit = new string[5];
+            string[] fruit = new string[5] { "an apple", "a banana", "a lemon", "an avacado", "an orange" };
+
+            //Console.WriteLine(fruit[0]);
+            //Console.WtiteLine(fruit[1]);
+            //...
+
+            //Console.WriteLine("enter a number from 0 to 4:");
+            //int userName = Convert.ToInt32(Console.Readline());
+            Random rnd = new Random();
+            int userNumber = rnd.Next(0, fruit.Length);
+
+            Console.WriteLine($"Today you will have fruit{fruit[userNumber]}.");
         }
     }
 }
